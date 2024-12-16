0620acece65eda458e36ee2fdf328c944fbe205f
Charlie-robin <charlie.robin.richardson@gmail.com>
2024-12-16 08:38:37 +0000

    Day 3 exercises

--- exercises/pokeAPI.js
+++ exercises/pokeAPI.js
@@ -1,3 +1,9 @@
 export async function getPokemonList() {
-  // TODO
+  const request = await fetch(
+    'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
+  )
+  const json = await request.json()
+  const names = json.results.map((result) => result.name)
+
+  return names
 }
--- exercises/readJSONTransactions.js
+++ exercises/readJSONTransactions.js
@@ -1,7 +1,10 @@
 import fs from 'fs/promises'
 
 export async function readJSONTransactions() {
-  // Read the Transactions2013.json file
-  // Use JSON.parse to convert the string into an array of objects
-  // and return it
+  const path = new URL('../data/Transactions2013.json', import.meta.url)
+  const content = await fs.readFile(path, 'utf-8')
+  return JSON.parse(content)
 }
+// Read the Transactions2013.json file
+// Use JSON.parse to convert the string into an array of objects
+// and return it
